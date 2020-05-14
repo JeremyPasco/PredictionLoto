7feacc4187d5bcf9ae6ce95ede337fee003902ca
JeremyPasco <32913603+JeremyPasco@users.noreply.github.com>
2020-05-14 16:24:44 +0200

    Ou plus simplement :D

--- fonctions_secretes_du_loto/g.py
+++ fonctions_secretes_du_loto/g.py
@@ -1,7 +1,3 @@
 # Retourne la valeur absolue de l'argument nombre
 def g(nombre):
-    if nombre<0 :
-        abs = nombre*(-1)
-    else : 
-        abs = nombre
-    return(abs)
+    return abs(nombre
